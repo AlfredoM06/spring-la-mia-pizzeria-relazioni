b76941fd0708c948a9b45d685011f833fac8f76a
AlfredoM06 <Alfredomanfredi06@gmail.com>
2024-01-09 23:25:55 +0100

    Initial commit

---
+++ src/main/java/org/lessons/springlamiapizzeriacrud/Model/Pizza.java
@@ -0,0 +1,46 @@
+package org.lessons.springlamiapizzeriacrud.Model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "pizze")
+public class Pizza {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String name;
+    private String description;
+    private Integer price;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Integer getPrice() {
+        return price;
+    }
+
+    public void setPrice(Integer price) {
+        this.price = price;
+    }
+}
